5864bb807775a0a53f90f803479ffc36e09586a3
37095studentwsei <37095@studentwsei.pl>
2022-03-19 15:01:32 +0100

    Dodawanie liczb wprowadzonych przez użytkownika po wstawieniu deklaracji importu tj. import java.util.Scanner

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,5 +1,7 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -26,11 +28,24 @@ public class Main {
 
         System.out.printf("%s",s);
 
+        Scanner input = new Scanner(System.in);
+        int num1=0, num2=0;
+        do {
+            System.out.print("Podaj pierwsza liczbe");
+            num1 = input.nextInt();
+            System.out.print("Podaj druga liczbe");
+            num2 = input.nextInt();
 
+            if (num1 == 0) break;
+            System.out.printf("Wynik dodawania %d + %d = %d%n,",num1,num2,num1+num2);
+        }while (true);
+
+
+        }
 
 
     }
-}
+
 /**
  * @author Jakub Cieżko
  * @verison 1.0
