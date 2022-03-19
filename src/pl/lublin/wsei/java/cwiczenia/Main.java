0230ffaf0434c4586158051d9916a9a9101c6668
37095studentwsei <37095@studentwsei.pl>
2022-03-19 14:44:44 +0100

    Dodanie zmiennych

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -5,5 +5,15 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("Hello Java World\n"); //komentarz
     System.out.println("Ala ma kota");
+
+    int a =3;
+    double b =4.21;
+    String s = "jakiś tekst\n";
+
+    System.out.printf("a = %d,b = %f, s= %s %n",a,b,s);
     }
 }
+/**
+ * @author Jakub Cieżko
+ * @verison 1.0
+ **/
